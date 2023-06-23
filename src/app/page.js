c366fb9cea7d879f315ad927bc1f309c1f3f9aef
ghousdar <hanifghous@gmail.com>
2023-06-23 15:07:33 +0500

    webp,jpg,png

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import mountains from "./images/mountain1.webp";
-
+import img2 from "./images/image2.jpg"
+import image3 from "./images/image3.png"
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -14,8 +15,30 @@ export default function Home() {
       width={500}
       height={500}
     /> */}
+
         <Image
+          style={{ padding: "4px" }}
           src={mountains}
+          className="w-100 shadow-1-strong rounded mb-4 "
+          alt="mountain"
+          priority
+          width={500}
+          height={500}
+        />
+
+        <Image
+          style={{ padding: "4px" }}
+          src={img2}
+          className="w-100 shadow-1-strong rounded mb-4 "
+          alt="mountain"
+          
+          width={300}
+          height={150}
+        />
+
+        <Image
+          style={{ padding: "4px" }}
+          src={image3}
           className="w-100 shadow-1-strong rounded mb-4 "
           alt="mountain"
           width={500}
@@ -23,9 +46,8 @@ export default function Home() {
         />
         <br></br>
 
-        <video controls={true} style={{ width: "400px", height:"400px" }}>
-          <source src="/video.mp4" type="video/mp4"  />
-          
+        <video controls={true} style={{ width: "400px", height: "400px" }}>
+          <source src="/video.mp4" type="video/mp4" />
         </video>
       </div>
     </main>
